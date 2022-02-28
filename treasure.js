3d725f09fc750c2b18389f4e15a6524f2e856f06
ecostrong <100543193+ecostrong@users.noreply.github.com>
2022-02-27 20:46:31 -0400

    Add files via upload

---
+++ treasure.js
@@ -0,0 +1,28 @@
+//alert ("Hey I am connected")
+console.log("connected")
+
+var randomLocation = Math.floor(Math.random() * 8)
+var randomBomb = Math.floor(Math.random() * 8)
+
+//const treasure = () => {
+if (randomLocation === randomBomb) {
+  randomBomb = Math.floor(Math.random() * 8)}
+ } else if(randomNum === 1) {
+// return document.getElementByID('treasure').innerHTML ='💣 '}
+// } else (randomNum === 1) {
+// return document.getElementByID(id).innerHTML ='🌈'}
+
+
+console.log(randomLocation,randomBomb)
+//document.getElementByID(id).innerHTML = '💣';
+//
+
+
+
+
+
+
+
+
+//https://www.wallpaperflare.com/static/264/160/929/art-underwater-sea-fish-wallpaper.jpg
+//}
